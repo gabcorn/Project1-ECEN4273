424f70c8af822f0ba2b6be51546505513a218a47
QuentinKoeninger <qkoenin@okstate.edu>
2025-02-25 11:22:31 -0600

    Pause and Settings maintenance
    Updated it so the pause menu will work now, also edited it so that the movement uses the input map instead of ui inputs, so the settings will now update the movement inputs.

--- movement.gd
+++ movement.gd
@@ -19,7 +19,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction := Input.get_axis("ui_left", "ui_right")
 	
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var direction = Input.get_vector("Left","Right","Up","Down")
 	velocity = direction * SPEED
 
 		
