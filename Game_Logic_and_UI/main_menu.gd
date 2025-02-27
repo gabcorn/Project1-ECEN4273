1ebe6227a9e00c78ee561dc24d8a5a66fdf06f21
Jacob Hood <JHoodCS@okstate.edu>
2025-02-27 13:42:25 -0600

    Organized the insanity of the files. Double check file paths now!

--- Game_Logic_and_UI/main_menu.gd
+++ Game_Logic_and_UI/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("Map.tscn")
+	get_tree().change_scene_to_file("res://Map_Related/Map.tscn")
 	
 
 func _on_settings_button_pressed() -> void:
--- health_bar.gd
+++
@@ -1,8 +0,0 @@
-extends HBoxContainer
-
-func _ready() -> void:
-	pass
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
