9d232ab22e17d4a5b96bda241480d240552e9558
Jacob Hood <JHoodCS@okstate.edu>
2025-02-28 21:49:16 -0600

    Healthbar is instantiating properly, need to add damage function

--- Character_Related/movement.gd
+++ Character_Related/movement.gd
@@ -5,19 +5,24 @@ const SPEED = 10.0
 var health_list : Array[TextureRect]
 var max_health = 9
 var curr_health = max_health * 2
+var SSN: Array[int] = []
 #const JUMP_VELOCITY = -400.0
 
 func _ready() -> void:
-	var health_bar = $CanvasLayer/Health_Bar
-	for child in health_bar.get_children():
-		health_list.append(child)
-	print(health_list)
+	generate_random_SSN()
+	update_health_bar()
 
+func generate_random_SSN() -> void: # Sets the characters SSN to a random number
+	for i in range(9):
+		SSN.append(randi())
+	
 func take_damage(damage: int) -> void:
 	if (curr_health - damage) > 0:
 		curr_health -= damage
 		update_health_bar()
 	else: #(curr_health - damage) <= 0
+		curr_health = 0
+		update_health_bar()
 		die()
 
 func heal_player(inc_health: int) -> void:
@@ -28,16 +33,46 @@ func heal_player(inc_health: int) -> void:
 		curr_health = max_health
 	else: # Increment current health by the amount to be healed by
 		curr_health += inc_health
+	update_health_bar()
 
 func die() -> void:
 	# Implement death logic
 	pass
 
 func update_health_bar() -> void:
-	# TODO: Implement health bar update logic
-	# Clear the current health bar
+	var health_bar = $CanvasLayer/Health_Bar
+	var num_full_hearts = curr_health / 2  # Number of full hearts to end up in the healthbar
+	var num_half_hearts = curr_health % 2 # Will be either 0 or 1
+	# Dictionary to convert easily between digits of the SSN and sprites in the healthbar
+	var HBar_to_SSN = {
+		0: 0,
+		1: 1,
+		2: 2,
+		3: 3,
+		5: 4,
+		6: 5,
+		8: 6,
+		9: 7,
+		10: 8
+	}
 	
-	pass
+	var children = health_bar.get_children()
+	for i in range(len(children)):
+		
+		var index = len(children) - 1 - i # Grabs the index from right to left
+		
+		var child = children[index].get_children()[0]
+		
+		if num_full_hearts == 0 and num_half_hearts == 0: # Set the animation of current node to SSN animation
+			if index != 4 && index != 7: # Ensure current character is an asterisk
+				var num = SSN[HBar_to_SSN[index]]
+				child.play(str(num))
+		elif (num_full_hearts > 0) and (child.animation != "IdleFull"):
+			num_full_hearts -= 1
+			child.play("IdleFull")
+		elif num_full_hearts == 0 and num_half_hearts == 1 and (child.animation != "IdleHalf"):
+			num_half_hearts -= 1
+			child.play("IdleHalf")
 
 func _physics_process(delta: float) -> void:
 	##no gravity for top down
